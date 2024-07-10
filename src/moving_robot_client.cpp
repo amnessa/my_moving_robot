d8af10e1596105eadf490176796f394646e0c1ff
cagolin2 <cagdas96vkp@gmail.com>
2024-07-10 13:13:40 +0300

    moving robot server completed and worked on client

--- src/moving_robot_client.cpp
+++ src/moving_robot_client.cpp
@@ -1,20 +1,91 @@
 #include "rclcpp/rclcpp.hpp"
+#include "rclcpp_action/rclcpp_action.hpp"
+#include "my_moving_robot_interfaces/action/location_speed.hpp"
+#include "example_interfaces/msg/empty.hpp"
 
+using MovingRobot = my_moving_robot_interfaces::action::LocationSpeed;
+using MovingRobotGoalHandle = rclcpp_action::ClientGoalHandle<MovingRobot>;
+using namespace std::placeholders;
 
-class MyCustomNode : public rclcpp::Node //MODIFY NAME
+class MovingRobotClientNode : public rclcpp::Node 
 {
-private:
-    /* data */
-public:
-    MyCustomNode() :Node("node_name"){
 
-    } //MODIFY NAME
+public:
+    MovingRobotClientNode() :Node("moving_robot_client")
+    {
+        moving_robot_client_=
+            rclcpp_action::create_client<MovingRobot>(this,"location_speed");
+
+        cancel_subscriber_ = this->create_subscription<Empty>(
+            "cancel_move",10,std::bind(&MovingRobotClientNode::callback_cancel_move,this,_1));
+    }
+
+    void send_goal(int position, int velocity)
+    {
+       moving_robot_client_->wait_for_action_server();
+
+       auto goal = MovingRobot::Goal();
+       goal.position = position;
+       goal.velocity=velocity;
+
+       auto options = rclcpp_action::Client<MoveRobot>::SendGoalOptions();
+        // add all binds
+
+    }
+private:
+    void timer_callback()
+    {
+        RCLCPP_INFO(this->get_logger(),"Cancel the goal");
+        moving_robot_client_->async_cancel_goal(goald_handle_);
+        
+    }
+    // callback to check goal situation
+    void goal_response_callback(const MovingRobotGoalHandle::SharedPtr &goal_handle)
+    {
+        if(!goal_handle)
+        {
+            RCLCPP_INFO(this->get_logger(), "Goal got rejected");
+        }else
+        {
+            this->goal_handle_ =goal_handle;
+            RCLCPP_INFO(this->get_logger(), "Goal got accepted"); 
+        }
+    }
+    //callback for result
+    void goal_result_callback(const MovingRobotGoalHandle::WrappedResult &result)
+    {
+        auto status =result.code;
+        if(status == rclcpp_action::ResultCode::SUCCEEDED)
+        {
+            RCLCPP_INFO(this->get_logger(), "Succeeded"); 
+        }else if(status == rclcpp_action::ResultCode::ABORTED)
+        {
+            RCLCPP_ERROR(this->get_logger(), "Aborted"); 
+        }else if(status == rclcpp_action::ResultCode::CANCELED)
+        {
+            RCLCPP_WARN(this->get_logger(), "Cancelled");
+        }
+        int position = result.result->position;
+        RCLCPP_INFO(this->get_logger(),"Result: %d",position);
+
+    }
+    // callback for feedback during execution
+    void goal_feedback_callback(const MovingRobotGoalHandle::SharedPtr &goal_handle,
+        const std::shared_ptr<MovingRobot::Feedback> feedback)
+    {
+        (void)goal_handle;
+        (void)feedback;
+    }
+    rclcpp_action::Client<MovingRobot>::SharedPtr moving_robot_client_;
+    MovingRobotGoalHandle::SharedPtr goal_handle_;
+    rclcpp::Subscription<Empty>::SharedPtr cancel_subscriber_;
+
 };
 
 
 int main(int argc,char **argv){
     rclcpp::init(argc,argv);
-    auto node = std::make_shared<MyCustomNode>(); //MODIFY NAME
+    auto node = std::make_shared<MovingRobotClientNode>(); 
     rclcpp::spin(node);
     rclcpp::shutdown();
 
--- src/moving_robot_server.cpp
+++ src/moving_robot_server.cpp
@@ -1,8 +1,11 @@
 #include "rclcpp/rclcpp.hpp"
 #include "rclcpp_action/rclcpp_action.hpp"
-//#include "my_robot_interfaces/action/LocationSpeed.hpp"
+#include "my_moving_robot_interfaces/action/location_speed.hpp"
 
-using MovingRobot= my_robot_interfaces::action::LocationSpeed;
+using MovingRobot= my_moving_robot_interfaces::action::LocationSpeed;
+using namespace std::placeholders;
+using MovingRobotGoalHandle= rclcpp_action::ServerGoalHandle<MovingRobot>;
+
 
 class MovingRobotServerNode : public rclcpp::Node 
 {
@@ -10,18 +13,168 @@ class MovingRobotServerNode : public rclcpp::Node
 public:
     MovingRobotServerNode() :Node("moving_robot_server")
     {
+        robot_position_ = 50;
+        cb_group_=this->create_callback_group(rclcpp::CallbackGroupType::Reentrant);
+        moving_robot_server_=rclcpp_action::create_server<MovingRobot>(
+            this,
+            "location_speed",
+            std::bind(&MovingRobotServerNode::goal_callback,this, _1,_2),
+            std::bind(&MovingRobotServerNode::cancel_callback,this,_1),
+            std::bind(&MovingRobotServerNode::handle_accepted_callback,this,_1),
+            rcl_action_server_get_default_options(),
+            cb_group_
+        );
+        RCLCPP_INFO(this->get_logger(), "Action server has been started");
+        RCLCPP_INFO(this->get_logger(), "Robot position: %d", robot_position_);
 
     } 
 
 private:
-    rclcpp_action::Server<CountUntil>::SharedPtr count_until_server_;
+    rclcpp_action::GoalResponse goal_callback(
+        const rclcpp_action::GoalUUID &uuid, std::shared_ptr<const MovingRobot::Goal> goal)
+    {
+        (void)uuid;
+        //(void)goal;
+        RCLCPP_INFO(this->get_logger(),"Received a goal");
+
+        // goal validation
+        if ( (goal->position<0)||(goal->position>100)||(goal->velocity<=0))
+        {
+            RCLCPP_INFO(this->get_logger(), "Invalid position/velocity, reject goal");
+            return rclcpp_action::GoalResponse::REJECT;
+        }
+        
+        //preemtping after getting a valid goal
+        {
+            std::lock_guard<std::mutex>lock(mutex_);
+            if(goal_handle_)
+            {
+                if(goal_handle_->is_active())
+                {
+                    RCLCPP_INFO(this->get_logger(),"Abort current goal and accept new goal");
+                    preempted_goal_id_=goal_handle_->get_goal_id();
+                }
+            }
+        }
+        RCLCPP_INFO(this->get_logger(),"Accepting the goal");
+        return rclcpp_action::GoalResponse::ACCEPT_AND_EXECUTE;
+    }
+
+    rclcpp_action::CancelResponse cancel_callback(
+        const std::shared_ptr<MovingRobotGoalHandle> goal_handle)
+    {
+        RCLCPP_INFO(this->get_logger(),"Received cancel request");
+        (void)goal_handle;
+        return rclcpp_action::CancelResponse::ACCEPT;
+    }
+
+    void handle_accepted_callback(
+        const std::shared_ptr<MovingRobotGoalHandle> goal_handle)
+    {
+        RCLCPP_INFO(this->get_logger(), "Executing the goal");
+        execute_goal(goal_handle);
+    }
+
+    void execute_goal(const std::shared_ptr<MovingRobotGoalHandle> goal_handle)
+    {
+        {
+            std::lock_guard<std::mutex>lock(mutex_);
+            this->goal_handle_=goal_handle;
+        }
+        //main logic here
+
+        //get request from goal
+        int goal_position = goal_handle->get_goal()->position;
+        int velocity = goal_handle->get_goal()->velocity;
+        int total_interval = 100;
+        int total_distance = 100;
+
+
+        // execute the action
+
+        int counter=0;
+        auto result = std::make_shared<MovingRobot::Result>();
+        auto feedback = std::make_shared<MovingRobot::Feedback>();
+        rclcpp::Rate loop_rate(1.0);
+        std::this_thread::sleep_for(std::chrono::milliseconds(50));
+        
+        RCLCPP_INFO(this->get_logger(), "Execute goal");
+        while (rclcpp::ok()) {
+            // Check if needs to preempt goal
+            {
+                std::lock_guard<std::mutex> lock(mutex_);
+                if (goal_handle->get_goal_id() == preempted_goal_id_) {
+                    result->position = robot_position_;
+                    result->message = "Preempted by another goal";
+                    goal_handle->abort(result);
+                    return;
+                }
+            }
+
+            // Check if cancel request
+            if (goal_handle->is_canceling()) {
+                result->position = robot_position_;
+                if (goal_position == robot_position_) {
+                   result->message = "Success";
+                   goal_handle->succeed(result); 
+                }
+                else {
+                    result->message = "Canceled";
+                    goal_handle->canceled(result);
+                }
+                return;
+            }
+
+            int diff = goal_position - robot_position_;
+
+            if (diff == 0) {
+                result->position = robot_position_;
+                result->message = "Success";
+                goal_handle->succeed(result);
+                return;
+            }
+            else if (diff > 0) {
+                if (diff < velocity) {
+                    robot_position_ += diff;
+                }
+                else {
+                    robot_position_ += velocity;
+                }
+            }
+            else if (diff < 0) {
+                if (abs(diff) < velocity) {
+                    robot_position_ -= abs(diff);
+                }
+                else {
+                    robot_position_ -= velocity;
+                }
+            }
+
+            RCLCPP_INFO(this->get_logger(), "Robot position: %d", robot_position_);
+            feedback->current_position = robot_position_;
+            goal_handle->publish_feedback(feedback);
+
+            loop_rate.sleep();
+        }
+          
+    }
+
+    rclcpp_action::Server<MovingRobot>::SharedPtr moving_robot_server_;
+    rclcpp::CallbackGroup::SharedPtr cb_group_;
+    std::shared_ptr<MovingRobotGoalHandle> goal_handle_;
+    int robot_position_;
+    std::mutex mutex_;
+    rclcpp_action::GoalUUID preempted_goal_id_;
 };
 
 
 int main(int argc,char **argv){
     rclcpp::init(argc,argv);
     auto node = std::make_shared<MovingRobotServerNode>(); 
-    rclcpp::spin(node);
+    rclcpp::executors::MultiThreadedExecutor executor;
+    executor.add_node(node);
+    executor.spin();
+    //rclcpp::spin(node);
     rclcpp::shutdown();
 
     return 0;
